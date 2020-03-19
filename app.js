493fbdb0af23a33b43f819edf781070527e3b08f
Louis Lebrault <louis.lebrault@gmail.com>
2020-03-19 19:01:43 +0100

    authent to cloudflow, add comments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const ldap = require('ldap');
 const http = require('http');
+const fetch = require('node-fetch');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -20,6 +21,8 @@ server.listen(port, hostname, () => {
   const client = ldap.createClient({
     url: 'ldap://10.10.10.10:389'
   });
+
+  // =================== Test utilisation LDAP CACOM =========================
 
   client.bind(USER_MAIL, USER_PASSWORD, (err, res) => {
     if (err) {
@@ -52,4 +55,32 @@ server.listen(port, hostname, () => {
       });
     });
   })
+
+  // ===================== Test authent Cloudflow ==============================
+
+  fetch('http://ptitniko.freeboxos.fr:9090/portal.cgi', {
+    method: 'POST',
+    body: JSON.stringify({
+      method: 'auth.create_session',
+      user_name: 'admin',
+      user_pass: 'im3dsAYT3PeNDp9',
+    })
+  }).then(res => {
+    res.json().then(json => {
+      // ce json contient la sessionId qu'il faudra envoyer au front ou stocker dans un cookie httpOnly
+      console.log('SUCCESS :', json)
+    })
+  }).catch(e => {
+    console.log('ERROR :', err)
+  })
+
+  // C'est tout simple. Juste a voir la question de la synchro du LDAP entre cloudflow et cacom.
+  // Chaque requete venant du front, si elle doit taper sur cloudflow, devra contenir le sessionId.
+
+  // Faudra tester, quand on envoi un sessionId expiré, quel est la réponse de Cloudflow. Faudra a ce moment là se réauthentifié :
+  // On a pas envie que l'user rerentre ses logs a chaque fois que le sessionId de cloudflow expire. L'ideal serait donc d'authentifié l'utilisateur
+  // avec un token jwt contenant son username et son password : si cloudflow dit que la session est expiré, l'api aurait deja toutes les infos pour le
+  // réauthentifié auprès de cloudflow
+
+  // sessionId datant de 2020/03/19 à 18h53 : 5e6ba3e8dbb1c6374d0d2de721B7322CBE1007CEF60C8423AEA0277A1584683625
 });
